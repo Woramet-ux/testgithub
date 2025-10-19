87eee72a3e825aa9a187f15dda550a709ac571d3
deadbrainwaking <s6806021411481@email.kmutnb.ac.th>
2025-10-19 18:17:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const sections = document.querySelectorAll('.section-fade-in');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('is-visible');
+            }
+        });
+    }, {
+        threshold: 0.1 
+    });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+
+});
